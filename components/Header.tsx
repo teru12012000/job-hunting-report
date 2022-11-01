4dbe966df0d2b7701a69c6e59fa92b6a9f9bc0aa
teru12012000 <sion0012@outlook.jp>
2022-11-01 17:44:58 +0900

    add twitterIcon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 import { NextPage } from "next";
 import head from "./styles/Header.css";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Link from "next/link";
 const Header: NextPage= () => {
   return (
     <header className={head.content}>
-      <h1>就活レポート</h1>
+      <h1 className={head.h1}>就活レポート</h1>
+      <Link href="https://twitter.com/TERUSI1201">
+        <a target="_blank" className={head.a}><TwitterIcon className={head.icon}/></a>
+      </Link>
     </header>
   );
 }
--- components/styles/Header.css.ts
+++ components/styles/Header.css.ts
@@ -6,12 +6,31 @@ const head={
     color:"white",
     padding:"3px",
     fontSize:"15px",
+    width:"100%",
+    height:"90px",
+    display:"flex",
+    justifyContent:"space-between",
     '@media':{
       'screen and (max-width:400px)':{
         fontSize:"8px"
       }
     }
   }),
+  h1:style({
+    marginTop:"20px",
+    fontSize:"30px",
+  }),
+  a:style({
+    marginTop:"20px",
+    marginRight:"10px",
+  }),
+  icon:style({
+    fontSize:"30px",
+    ":hover":{
+      color:"aqua",
+      transition:"0.6s",
+    }
+  })
 }
 
 export default head
