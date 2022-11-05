f29fce91cc496cecda4822e78678aecf861f34cf
teru12012000 <sion0012@outlook.jp>
2022-11-05 12:35:39 +0900

    change tag

--- pages/selection.tsx
+++ pages/selection.tsx
@@ -62,7 +62,7 @@ const selection: NextPage= () => {
     },
   ]
   return (
-    <html lang="ja">
+    <>
       <Head>
         <title>選考について</title>
       </Head>
@@ -133,7 +133,7 @@ const selection: NextPage= () => {
         </p>
         </div>
       </div>
-    </html>
+    </>
     
   );
 }
