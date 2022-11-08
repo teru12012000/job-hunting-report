a6ee2497a68317aa88d5e55e8656c3d67505c6f1
teru12012000 <sion0012@outlook.jp>
2022-11-08 16:58:54 +0900

    add component

--- components/Olist.tsx
+++ components/Olist.tsx
@@ -1,13 +1,14 @@
 import { ollist } from "../pages/interview";
+import {FC} from "react";
 import int_sty from "../styles/interview.css";
 type Props={
   list:ollist[];
 }
-const Olist = (props:Props) => {
+const Olist:FC<Props> = ({list}) => {
   return (
     <>
     <ol className={int_sty.ol}>
-          {props.list.map((item,index)=>(
+          {list.map((item,index)=>(
             <li key={index}>{item.menu}</li>  
           ))}
           </ol>
